3d6d1a04f98fd681ed5a076fc06343491825f228
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-05 17:06:17 +0300

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import Router from './router'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from "pinia"
+import vuetify from "@/plugins/vuetify"
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(Router)
 app.use(pinia)
+app.use(vuetify)
 
 app.mount('#app')
