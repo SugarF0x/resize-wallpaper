6b9f675908d7849329b5f3b113f4f9c8469f2b70
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-05 12:31:59 +0300

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import router from './routes'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from "pinia"
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
