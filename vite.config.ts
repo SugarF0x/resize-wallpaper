e3d9c56efab7a2c8ad1290eb800fbe322ad4d6a4
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-12 15:35:00 +0300

    add runetime type checker

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import vuetify from "vite-plugin-vuetify"
+import checker from "vite-plugin-checker"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vuetify()
+    vuetify(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
   ],
   resolve: {
     alias: {
